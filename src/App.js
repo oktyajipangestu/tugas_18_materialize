74d20dee60c15d023c88a761410e714dd054b14a
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-05 08:47:47 +0700

    menambahkan pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { Row, Col, Card, CardTitle, Slider, Slide, Caption, Pagination, Icon } from "react-materialize";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Slider
+          fullscreen={false}
+          options={{
+            duration: 500,
+            height: 400,
+            indicators: true,
+            interval: 6000,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Slide
+            image={
+              <img
+                alt=""
+                src="https://s-ec.bstatic.com/images/hotel/max1024x768/106/106114417.jpg"
+              />
+            }
+          >
+            <Caption placement="center">
+              <h3>Pantai</h3>
+              <h5 className="light grey-text text-lighten-3">
+                Dapatkan Kesempatan Jalan -Jalan Ke Pantai Dengan Membeli Sebuah
+                Product
+              </h5>
+            </Caption>
+          </Slide>
+          <Slide
+            image={
+              <img
+                alt=""
+                src="https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?wid=2880&hei=1280&fmt=jpeg&crop=9,336,2699,1200&anchor=1358,936&qlt=75,0&fit=wrap&op_sharpen=0&resMode=sharp2&op_usm=0,0,0,0&iccEmbed=0&printRes=72"
+              />
+            }
+          >
+            <Caption placement="center">
+              <h3>Eiffel</h3>
+              <h5 className="light grey-text text-lighten-3">
+                Dapatkan Kesempatan Jalan -Jalan Ke Pantai Dengan Membeli Sebuah
+                Product
+              </h5>
+            </Caption>
+          </Slide>
+          <Slide
+            image={<img alt="" src="https://lorempixel.com/780/580/nature/3" />}
+          >
+            <Caption placement="center">
+              <h3>Pegunungan</h3>
+              <h5 className="light grey-text text-lighten-3">
+                Dapatkan Kesempatan Jalan -Jalan Ke Pantai Dengan Membeli Sebuah
+                Product
+              </h5>
+            </Caption>
+          </Slide>
+        </Slider>
+
+        <p>Hot List</p>
+
+        <Row>
+          <Col m="3">
+            <Card
+              header={
+                <CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/8/15/25749302/25749302_274a89a6-ea41-4a3a-ac6b-ce2881016369.jpg">
+                  Audio
+                </CardTitle>
+              }
+            >
+              Audio Technica - Mulai dari <span className="red">Rp 980rb</span>
+            </Card>
+          </Col>
+          <Col m="3">
+            <Card
+              header={
+                <CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2019/1/16/25287362/25287362_a79f0aa6-cf1f-484f-b1a8-83993245cd6f.jpg">
+                  Handphone
+                </CardTitle>
+              }
+            >
+              Handphone Samsung - Mulai dari{" "}
+              <span className="red">Rp 200rb</span>
+            </Card>
+          </Col>
+          <Col m="3">
+            <Card
+              header={
+                <CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/6/26/28201197/28201197_856195f2-b0ba-4711-8200-fd95beaf1df5.jpg">
+                  Sequishy
+                </CardTitle>
+              }
+            >
+              Sequishy - Mulai dari <span className="red">Rp 5,5rb</span>
+            </Card>
+          </Col>
+          <Col m="3">
+            <Card
+              header={
+                <CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/4/23/28201197/28201197_0383072a-1766-453c-951a-8b53a719cff0.jpg">
+                  Koleksi The Avengers
+                </CardTitle>
+              }
+            >
+              Koleksi The Avengers - Mulai dari{" "}
+              <span className="red">Rp 10rb</span>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Pagination
+              activePage={1}
+              items={10}
+              leftBtn={<Icon>chevron_left</Icon>}
+              maxButtons={8}
+              rightBtn={<Icon>chevron_right</Icon>}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
